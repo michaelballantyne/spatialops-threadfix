92b1843117579ca21afe4ae42163bb0f5a83122d
Naveen Punati <naveen.punati@gmail.com>
2010-12-22 18:40:21 -0700

    Updated particle operators

--- spatialops/particles/ParticleOperators.h
+++ spatialops/particles/ParticleOperators.h
@@ -2,38 +2,40 @@
 #define ParticleOperators_h
 
 #include <spatialops/particles/ParticleFieldTypes.h>
+#include <math.h>
 
 namespace SpatialOps{
 namespace Particle{
 
-//   /**
-//    *  Interpolates a particle field onto an underlying mesh field.
-//    *
-//    *  JCS: it isn't clear how to do this appropriately since we may
-//    *  have multiple particles in a single cell...
-//    */
-//   template< typename CellField >
-//   class ParticleToCell
-//   {
-//   public:
-//     typedef ParticleField SrcFieldType;
-//     typedef CellField     DestFieldType;
+   /**
+    *  Interpolates a particle field onto an underlying mesh field.      
+    */
+   template< typename CellField >
+   class ParticleToCell
+   {
+   public:
+     typedef ParticleField SrcFieldType;
+     typedef CellField     DestFieldType;
 
-//     /**
-//      *  @param particleCoord Field of coordinates for all particles
-//      *  @param meshCoord Field of coordinates for the underlying mesh
-//      */
-//     ParticleToCell( const ParticleField& particleCoord,
-//                     const CellField& meshCoord );
+     /**
+      *  @param meshCoord Vector of coordinates for the underlying mesh
+      */
+     ParticleToCell( const std::vector<double>& meshCoord );
 
-//     void apply_to_field( const SrcFieldType&,
-//                          DestFieldType& ) const;
+     /**
+    * @param particleCoord Field of coordinates for all particles (ParticleField)
+    * @param src source field from which values are interpolated to partciles (ParticleField)
+    * @param dest destination field to which values are interpolated (CellField)
+    */
+     void apply_to_field( const ParticleField& particleCoord,
+                          const ParticleField& particleSize,
+                          const SrcFieldType& src,
+                          DestFieldType& dest ) const;
 
-//   private:
-//     const ParticleField& particleCoord_;
-//     const CellField& cellCoord_;
-//     const double dx_, xo_;
-//   };
+   private:
+     const std::vector<double>& Coordvec_;
+     const double dx_, xo_;
+   };
 
 
   //==================================================================
@@ -52,16 +54,22 @@ namespace Particle{
     typedef ParticleField DestFieldType;
 
     /**
-     *  @param particleCoord Field of coordinates for all particles
      *  @param meshCoord Field of coordinates for the underlying mesh
-     */
-    CellToParticle( const CellField& meshCoord );
+     */   
+    CellToParticle( const std::vector<double>& meshCoord ); 
 
+    /**
+    * @param particleCoord Field of coordinates for all particles (ParticleField)
+    * @param src source field from which values are interpolated to partciles (VolField)
+    * @param dest destination field to which values are interpolated (ParticleField)
+    */
     void apply_to_field( const ParticleField& particleCoord,
-                         const SrcFieldType&,
-                         DestFieldType& ) const;
+                         const SrcFieldType& src,
+                         DestFieldType& dest ) const;
+   
+
   private:
-    const CellField& cCoord_;
+    const std::vector<double>& Coordvec_;
     const double dx_, xo_;
   };
 
@@ -75,57 +83,65 @@ namespace Particle{
 
 
 
-  // template< typename CellField >
-//   ParticleToCell<CellField>::
-//   ParticleToCell( const ParticleField& particleCoord,
-//                   const CellField& meshCoord )
-//     : particleCoord_( particleCoord ),
-//       cellCoord_    ( meshCoord     ),
-//       dx_( meshCoord[1]-meshCoord[0] ),
-//       xo_( meshCoord[0] )
-//   {
-//     const double TOL = 1e-6;
+   template< typename CellField >
+   ParticleToCell<CellField>::
+   ParticleToCell( const std::vector<double>& meshCoord )
+     : Coordvec_   ( meshCoord     ),
+       dx_( meshCoord[1]-meshCoord[0] ),
+       xo_( meshCoord[0] )
+   {
+     const double TOL = 1e-6;
 
-//     // note that this assumes 1D
-//     bool isUniform = true;
-//     typename CellField::const_iterator ix2=meshCoord.begin();
-//     typename CellField::const_iterator ix = ix2++;
-//     for( ; ix2!=meshCoord.end(); ++ix, ++ix2 ){
-//       if( std::abs( dx_ - (*ix2-*ix) )/dx_ > TOL ){
-//         isUniform = false;
-//       }
-//     }
-//     assert( isUniform );
-//   }
+     // note that this assumes 1D
+     bool isUniform = true;
+     std::vector<double>::const_iterator  ix2=meshCoord.begin();
+     std::vector<double>::const_iterator  ix = ix2++;
+     for( ; ix2!=meshCoord.end(); ++ix, ++ix2 ){
+       if( fabs( dx_ - (*ix2-*ix) )/dx_ > TOL ){
+         isUniform = false;
+       }
+     }
+     assert( isUniform );
+   }
 
-//   //------------------------------------------------------------------
+   //------------------------------------------------------------------
 
-//   template< typename CellField >
-//   void
-//   ParticleToCell<CellField>::
-//   apply_to_field( const SrcFieldType& src,
-//                   DestFieldType& dest ) const
-//   {
-//     dest = 0.0;
-//     ParticleField::const_iterator ipx=particleCoord_.begin();
-//     ParticleField::const_iterator isrc = src.begin();
+   template< typename CellField >
+   void
+   ParticleToCell<CellField>::
+   apply_to_field( const ParticleField& particleCoord,
+                   const ParticleField& particleSize,
+                   const SrcFieldType& src,
+                   DestFieldType& dest) const
+   {
+     dest = 0.0;
+     ParticleField::const_iterator plociter = particleCoord.begin();
+     //ParticleField::const_iterator psizeiter = particleSize.begin();
+     ParticleField::const_iterator isrc = src.begin();
 
-//     const double halfwidth = 0.5*dx_;
+     const double halfwidth = 0.5*dx_;
 
-//     for( ; ipx!=particleCoord_.end(); ++ipx, ++isrc ){
-//       // given the current particle coordinate,
-//       // determine what cell it is located in.
-//       const size_t cellIx = (*ipx-halfwidth-xo_) / dx_ -1;
-//       dest[cellIx] = *isrc;
-//     }
-//   }
+     for( ; plociter!=particleCoord.end(); ++plociter, ++isrc ){
+       // given the current particle coordinate,
+       // determine what cell it is located in.
+       const size_t cellIx1 = size_t((*plociter-halfwidth-xo_) / dx_);
+       const size_t cellIx2 = cellIx1 +1 ;
+       const double leftloc = Coordvec_[cellIx1] ;
+       const double rightloc = Coordvec_[cellIx2] ;
+       //std::cout<<" cellIx1 : "<<cellIx1<<"  cellIx1 : "<<cellIx2<<"  leftloc  : "<< leftloc<<"  rightloc : "<<rightloc<<std::endl;
+       if( fabs( *plociter - leftloc) <= fabs( *plociter - rightloc ))
+          dest[cellIx1] += *isrc;
+       else
+          dest[cellIx2] += *isrc;
+     }
+   }
 
-  //==================================================================
-
+ 
+ //==================================================================
   template< typename CellField >
   CellToParticle<CellField>::
-  CellToParticle( const CellField& meshCoord )
-    : cCoord_( meshCoord ),
+  CellToParticle( const std::vector<double>& meshCoord )
+    : Coordvec_( meshCoord ),
       dx_( meshCoord[1]-meshCoord[0] ),
       xo_( meshCoord[0] )
   {
@@ -133,10 +149,10 @@ namespace Particle{
 
     // note that this assumes 1D
     bool isUniform = true;
-    typename CellField::const_iterator ix2=meshCoord.begin();
-    typename CellField::const_iterator ix = ix2; ++ix2;
-    for( ; ix2!=meshCoord.end(); ++ix, ++ix2 ){
-      if( std::abs( dx_ - (*ix2-*ix) )/dx_ > TOL ){
+    std::vector<double>::const_iterator ix2=meshCoord.begin();
+    std::vector<double>::const_iterator ix = ix2++;
+    for( ; ix2!=meshCoord.end(); ++ix, ++ix2 ){     
+      if( fabs( dx_ - (*ix2-*ix) )/dx_ > TOL ){
         isUniform = false;
       }
     }
@@ -163,17 +179,16 @@ namespace Particle{
       // is located in.  Then interpolate the src values (from the
       // mesh) onto the particle using linear interpolation
       const double xp = *ipx;
-      const size_t i1 = size_t( (xp-halfwidth-xo_) / dx_ -1 );
-      const double x1 = cCoord_[i1];
-      const size_t i2 = ( xp > x1 ) ? i1+1 : i1-1;
-      const double x2 = cCoord_[i2];
+      const size_t i1 = size_t( (xp-halfwidth-xo_) / dx_ );     
+      const double x1 = Coordvec_[i1];     
+      const size_t i2 = ( xp > x1 ) ? i1+1 : i1-1;     
+      const double x2 = Coordvec_[i2];      
       const double c1 = (xp-x2)/(x1-x2);
       const double c2 = (xp-x1)/(x2-x1);
       *idest = src[i1]*c1 + src[i2]*c2;
+
     }
   }
-
-  //------------------------------------------------------------------
 
 } // namespace Particle
 } // namespace SpatialOps
