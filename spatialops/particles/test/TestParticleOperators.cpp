402631a08d8ad15fcc5ce7d7a8d51777d8c84318
James C. Sutherland <James.Sutherland@utah.edu>
2011-05-16 12:11:10 -0600

    Fix bugs in test for particle operators.

--- spatialops/particles/test/TestParticleOperators.cpp
+++ spatialops/particles/test/TestParticleOperators.cpp
@@ -23,12 +23,11 @@ int main()
 {
   const size_t np=4;
   const double dx = 1.0;
-  std::vector<int> dim(3,1);
-  dim[0] = 10;
 
-  IntVec totDim(&dim[0]);
+  IntVec totDim(10,1,1);
   for( size_t i=0; i<3; ++i ) 
-    if( dim[i]>1 ) totDim[i] += 2*CellField::Ghost::NGHOST;
+    if( totDim[i]>1 ) totDim[i] += 2*CellField::Ghost::NGHOST;
+
   //
   // build the fields
   //
@@ -36,18 +35,16 @@ int main()
   CellField   ctmp( totDim, NULL );
 
   SpatialOps::Particle::ParticleField pCoord( IntVec(np,1,1), NULL );
-  SpatialOps::Particle::ParticleField pSize  ( IntVec(np,1,1), NULL );
+  SpatialOps::Particle::ParticleField pSize ( IntVec(np,1,1), NULL );
   SpatialOps::Particle::ParticleField pfield( IntVec(np,1,1), NULL );
   SpatialOps::Particle::ParticleField ptmp  ( IntVec(np,1,1), NULL );
    
-
   //
   // set the cCoord coordinates.  These go from -0.5 to 10.5
   //
-  CellField::iterator icoord = cellField.begin();
-  CellField::iterator icoordEnd = cellField.end();
-  for( size_t i=0; icoord != icoordEnd; ++icoord, ++i ){
-    //cellField[i] = i*2;
+  
+  size_t i=0;
+  for( CellField::iterator icoord=cellField.begin(); icoord!=cellField.end(); ++icoord, ++i ){
     *icoord = i*2 ;
   }
 
@@ -64,7 +61,6 @@ int main()
   pSize[2] = 0;
   pSize[3] = 0;
 
-
   pfield[0] = 10;
   pfield[1] = 20;
   pfield[2] = 40;
@@ -72,7 +68,6 @@ int main()
 
   for( size_t i=0; i<np; ++i )
     std::cout<<"  particle coord : "<<pCoord[i]<<"  particle field : "<<pfield[i]<<std::endl;
-
 
   //
   // build the operator
@@ -86,23 +81,27 @@ int main()
   // interpolate to particles
   //
   c2p->apply_to_field( pCoord, cellField, ptmp );
-  p2c->apply_to_field( pCoord,pSize, pfield, ctmp ); 
+  p2c->apply_to_field( pCoord, pSize, pfield, ctmp ); 
  
-
   for( size_t i=0; i<np; ++i )
     std::cout<<"  Interpolated particle field : "<<ptmp[i]<<std::endl;
 
-  icoord = ctmp.begin();
-  icoordEnd = ctmp.end();
-  for( size_t i=0; icoord != icoordEnd; ++icoord, ++i )
-    std::cout<<"  Interpolated Gas field : "<< *icoord <<std::endl;
+  CellField::const_iterator ix=cellField.begin();
+  for( CellField::const_iterator i=ctmp.begin(); i!=ctmp.end(); ++i, ++ix )
+    std::cout<<"  Interpolated Gas field at x=" << *ix << " = "<< *i <<std::endl;
 
   TestHelper status;
-  status( ptmp[0] == 3 );
-  status( ptmp[1] == 5.5 );
-  status( ptmp[2] == 3);
-  status( ptmp[3] == 16 );
+  for( size_t i=0; i<np; ++i )
+    status( ptmp[i] == pCoord[i] );
 
+  status( ctmp[0] == 50, "gas interp [0]" );
+  status( ctmp[1] == 20, "gas interp [1]" );
+  status( ctmp[2] ==  0, "gas interp [2]" );
+  status( ctmp[3] ==  0, "gas interp [3]" );
+  status( ctmp[4] == 70, "gas interp [4]" );
+  status( ctmp[5] ==  0, "gas interp [5]" );
+  status( ctmp[6] ==  0, "gas interp [6]" );
 
-  return status.ok();
+  if( status.ok() ) return 0;
+  return -1;
 }
