bdf4df60286b7e294917a4ab320f097164f8d19e
James C. Sutherland <James.Sutherland@utah.edu>
2011-05-16 14:43:10 -0600

    Add operators to move between regular fields and subset fields.

--- spatialops/pointfield/test/testPointField.cpp
+++ spatialops/pointfield/test/testPointField.cpp
@@ -1,8 +1,11 @@
 #include <spatialops/pointfield/PointFieldTypes.h>
+#include <spatialops/pointfield/PointOperators.h>
 
 #include <spatialops/structured/FVStaggeredBCTools.h>
 
 #include <test/TestHelper.h>
+
+#include <iostream>
 
 typedef SpatialOps::Point::PointField FieldT;
 
@@ -26,16 +29,38 @@ int main()
     status( &*i == &*i2, "address" );
   }
 
-  typedef SpatialOps::structured::ConstValEval BCVal;
-  typedef SpatialOps::structured::BoundaryCondition<FieldT,BCVal> BC;
+  {
+    typedef SpatialOps::structured::ConstValEval BCVal;
+    typedef SpatialOps::structured::BoundaryCondition<FieldT,BCVal> BC;
 
-  BC bc1( SpatialOps::structured::IntVec(2,1,1), BCVal(1.234) );
-  BC bc2( SpatialOps::structured::IntVec(4,1,1), BCVal(3.456) );
+    BC bc1( SpatialOps::structured::IntVec(2,1,1), BCVal(1.234) );
+    BC bc2( SpatialOps::structured::IntVec(4,1,1), BCVal(3.456) );
   
-  bc1(f);
-  bc2(f);
-  status( f[2] == 1.234, "point BC 1" );
-  status( f[4] == 3.456, "point BC 2" );
+    bc1(f);
+    bc2(f);
+    status( f[2] == 1.234, "point BC 1" );
+    status( f[4] == 3.456, "point BC 2" );
+  }
+
+  {
+    std::vector<size_t> ix;
+    ix.push_back(4);
+    ix.push_back(2);
+    SpatialOps::Point::FieldToPoint<FieldT> ftp(ix);
+    SpatialOps::Point::PointToField<FieldT> ptf(ix);
+
+    FieldT f2( SpatialOps::structured::IntVec(2,1,1), NULL );
+    ftp.apply_to_field( f, f2 );
+    status( f2[0] == 3.456, "Point2Field Interp (1)" );
+    status( f2[1] == 1.234, "Point2Field Interp (2)" );
+
+    f2[0] = -1.234;
+    f2[1] = -3.456;
+    ptf.apply_to_field( f2, f );
+    status( f[2] == -3.456, "Field2Point Interp (1)" );
+    status( f[4] == -1.234, "Field2Point Interp (2)" );
+  }
+
 
   if( status.ok() ) return 0;
   return -1;
