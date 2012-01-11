a767d6c811c54e3377c2445ea777fd10284f10ea
devin.robison <devin.robison@utah.edu>
2012-01-11 14:34:36 -0700

    Fixed correctness of opt2 stencil test, committing to test on alternate systems.

--- spatialops/structured/test/CudaStencilLib.h
+++ spatialops/structured/test/CudaStencilLib.h
@@ -26,11 +26,9 @@
 #define __shared__
 #endif
 
-
-
-
-// ( 256 threads per block )
 #define BLOCK_DIM 16
+#define INNER_BLOCK_DIM 14
+
 #define Index3D(_nx,_ny,_i,_j,_k) ((_i)+_nx*((_j)+_ny*(_k)))
 #define Index3DG(_nx, _ny, _g, _i, _j, _k) (((_i)+(_g)) + _nx*((_j)+(_g) + _ny*((_k)+(_g)) ))
 
@@ -44,31 +42,31 @@ void __global__ _div_float_slow( float* global_data_in,
   //Temporarily -> don't deal with computing boundary points
   if( i>0 && j>0 && (i<nx-1) && (j<ny-1) )
   {
-          for(int k=1;k<nz-1;k++)/* For each frame along the z-axis */
-          {
-                  float tijk = 2*global_data_in[ Index3D( nx, ny, i,j,k) ];
-                  global_data_out[Index3D (nx, ny, i, j, k)] =
-                  // X direction
-                  (
-                      global_data_in[Index3D (nx, ny, i - 1, j, k)] +
-                      global_data_in[Index3D (nx, ny, i + 1, j, k)] -
-                      tijk
-                  ) / ( dx * dx )
-                  +
-                  // Y direction
-                  (
-                      global_data_in[Index3D (nx, ny, i, j - 1, k)] +
-                      global_data_in[Index3D (nx, ny, i, j + 1, k)] -
-                      tijk
-                  ) / ( dy * dy )
-                  +
-                  // Z direction
-                  (
-                      global_data_in[Index3D (nx, ny, i, j, k - 1)] +
-                      global_data_in[Index3D (nx, ny, i, j, k + 1)] -
-                      tijk
-                  ) / ( dz * dz );
-          }
+    for(int k=1;k<nz-1;k++)/* For each frame along the z-axis */
+    {
+      float tijk = 2*global_data_in[ Index3D( nx, ny, i,j,k) ];
+      global_data_out[Index3D (nx, ny, i, j, k)] =
+      // X direction
+      (
+          global_data_in[Index3D (nx, ny, i - 1, j, k)] +
+          global_data_in[Index3D (nx, ny, i + 1, j, k)] -
+          tijk
+      ) / ( dx * dx )
+      +
+      // Y direction
+      (
+          global_data_in[Index3D (nx, ny, i, j - 1, k)] +
+          global_data_in[Index3D (nx, ny, i, j + 1, k)] -
+          tijk
+      ) / ( dy * dy )
+      +
+      // Z direction
+      (
+          global_data_in[Index3D (nx, ny, i, j, k - 1)] +
+          global_data_in[Index3D (nx, ny, i, j, k + 1)] -
+          tijk
+      ) / ( dz * dz );
+    }
   }
 }
 
@@ -84,40 +82,51 @@ void __global__ _div_float_opt1( float* global_data_in,
   zc = global_data_in[Index3D( nx, ny, i, j, 0)];
   zp1 = global_data_in[Index3D( nx, ny, i, j, 1 )];
 
-  if( i>0 && j>0 && (i<nx-1) && (j<ny-1) )
-  {
-          for(int k=1;k<nz-1;k++)/* For each frame along the z-axis */
-          {
-                  zm1 = zc;
-                  zc = zp1;
-                  zp1 = global_data_in[Index3D( nx, ny, i, j, k + 1 )];
-                  float tijk = 2*zc;
+  float dxsq = dx*dx;
+  //float dysq = dy*dy;
+  //float dzsq = dz*dz;
 
-                  global_data_out[Index3D (nx, ny, i, j, k)] =
-                  // X direction
-                  (
-                      global_data_in[Index3D (nx, ny, i - 1, j, k)] +
-                      global_data_in[Index3D (nx, ny, i + 1, j, k)] -
-                      tijk
-                  ) / ( dx * dx )
-                  +
-                  // Y direction
-                  (
-                      global_data_in[Index3D (nx, ny, i, j - 1, k)] +
-                      global_data_in[Index3D (nx, ny, i, j + 1, k)] -
-                      tijk
-                  ) / ( dy * dy )
-                  +
-                  // Z direction
-                  (
-                      zm1 +
-                      zp1 -
-                      tijk
-                  ) / ( dz * dz );
-          }
+  if( i > 0 && j > 0 && ( i < nx-1 ) && ( j < ny-1 ) ) {
+    for(int k = 1; k < nz-1 ; ++k)/* For each frame along the z-axis */ {
+      float x;
+      for( int data_reuse = 0; data_reuse < 1; ++data_reuse ){
+        zm1 = zc;
+        zc = zp1;
+        zp1 = global_data_in[Index3D( nx, ny, i, j, k + 1 )];
+        float tijk = 2*zc;
+
+        x =
+        (// X direction
+          (
+              global_data_in[Index3D (nx, ny, i - 1, j, k)] +
+              global_data_in[Index3D (nx, ny, i + 1, j, k)] -
+              tijk
+          )
+          +
+          // Y direction
+          (
+              global_data_in[Index3D (nx, ny, i, j - 1, k)] +
+              global_data_in[Index3D (nx, ny, i, j + 1, k)] -
+              tijk
+          )
+          +
+          // Z direction
+          (
+              zm1 +
+              zp1 -
+              tijk
+          )
+        ) / ( dxsq );
+      }
+      __syncthreads();
+      global_data_out[Index3D (nx, ny, i, j, k)] = x;
+    }
   }
 }
 
+// There appears to be a negative effect in making the effort to stage into shared
+// memory unless we can exhibit significant reuse in the shared data. Local reuse
+// between stencil points does not overcome the additional complexity of staging.
 void __global__ _div_float_opt2( float* global_data_in,
                                  float* global_data_out,
                                  float dx, float dy, float dz,
@@ -126,54 +135,89 @@ void __global__ _div_float_opt2( float* global_data_in,
   int ghost = 1; // static, because shared memory allocation has to be fixed.
 
   /** Compute inner tile variables **/
-  int inner_dim    = BLOCK_DIM;                         // Dimensions of the inner tile
-  int inner_x      = ( BlockIdx.x * inner_dim ) + ghost;// Inner tile absolute 'x' val
-  int inner_y      = ( BlockIdx.y * inner_dim ) + ghost;// Inner tile absolute 'y' val
+  int inner_dim    = INNER_BLOCK_DIM;                     // Dimensions of the inner tile
+  int inner_x      = ( blockIdx.x * inner_dim ) + ghost;// Inner tile absolute 'x' val
+  int inner_y      = ( blockIdx.y * inner_dim ) + ghost;// Inner tile absolute 'y' val
 
   /** Compute outer tile variables **/
-  int outer_dim    = BLOCK_DIM + 2 * ghost;             // Dimensions of the outer tile
-  int outer_x      = inner_x - ghost;                   // Outer tile absolute 'x' val
-  int outer_y      = inner_y - ghost;                   // Outer tile absolute 'y' val
+  //int outer_dim    = BLOCK_DIM;             // Dimensions of the outer tile
+  int outer_x      = inner_x - ghost;       // Outer tile absolute 'x' val
+  int outer_y      = inner_y - ghost;       // Outer tile absolute 'y' val
 
   // Cache the vertical stencil values zm1, z, zp1
   float zlow;
-  float zcenter;
-  float zhigh;
+  float zcenter = global_data_in[ Index3D(nx, ny, inner_x + threadIdx.x, inner_y + threadIdx.y, 0) ];
+  float zhigh   = global_data_in[ Index3D(nx, ny, inner_x + threadIdx.x, inner_y + threadIdx.y, 1) ];
 
-  __shared__ float local_data_in [BLOCK_DIM+2][BLOCK_DIM+2];
+  float dxsq = dx*dx;
+  float dysq = dy*dy;
+  float dzsq = dz*dz;
+
+  __shared__ float local_flat[BLOCK_DIM*BLOCK_DIM];
 
   for( int k = 1; k < nz - 1; ++k ) { // Iterate frames along the Z axis
     int inner_offset = Index3D(nx, ny, inner_x, inner_y, k);
     int outer_offset = Index3D(nx, ny, outer_x, outer_y, k);
-    float tijk = 0.01;
+    float tijk;
 
-    local_data_in[ThreadIdx.x][ThreadIdx.y] =
-        global_data_in[outer_offset + ThreadIdx.x + nx * ThreadIdx.y];
-
-    if( (ThreadIdx.x < BLOCK_DIM) && (ThreadIdx.y < BLOCK_DIM) ){
-
-      global_data_out[ inner_offset + ThreadIdx.x + ThreadIdx.y ] =
-          // X direction
-          (
-              data_in[Index3D (nx, ny, i - 1, j, k)] +
-              data_in[Index3D (nx, ny, i + 1, j, k)] -
-              tijk
-          ) / ( dx * dx )
-          +
-          // Y direction
-          (
-              data_in[Index3D (nx, ny, i, j - 1, k)] +
-              data_in[Index3D (nx, ny, i, j + 1, k)] -
-              tijk
-          ) / ( dy * dy )
-          +
-          // Z direction
-          (
-              data_in[Index3D (nx, ny, i, j, k - 1)] +
-              data_in[Index3D (nx, ny, i, j, k + 1)] -
-              tijk
-          ) / ( dz * dz );
+    if( ( (outer_x + threadIdx.x) < nx ) && ( (outer_y + threadIdx.y) < ny ) ){
+      local_flat[ Index3D(BLOCK_DIM, BLOCK_DIM, threadIdx.x, threadIdx.y, 0) ] =
+          global_data_in[outer_offset + threadIdx.x + nx * threadIdx.y];
     }
+
+    /*
+    if( k == 1 ){
+      d_debug[threadIdx.x + nx * threadIdx.y] =
+          local_flat[ Index3D( BLOCK_DIM, BLOCK_DIM, threadIdx.x, threadIdx.y, 0) ];
+    }
+    */
+
+    //Entire outer memory tile is loaded at this point.
+
+    if( (threadIdx.x < INNER_BLOCK_DIM )
+        && (threadIdx.y < INNER_BLOCK_DIM )
+        && ( inner_x + threadIdx.x < nx - 1 )
+        && ( inner_y + threadIdx.y < ny - 1 )){
+      int local_index_x = threadIdx.x + 1;
+      int local_index_y = threadIdx.y + 1;
+
+      zlow = zcenter;
+      zcenter = zhigh;
+      zhigh = global_data_in[ Index3D( nx, ny, inner_x + threadIdx.x, inner_y + threadIdx.y, k + 1 ) ];
+      tijk = 2 * zcenter;
+
+      float x;
+      for( int data_reuse=0; data_reuse < 1; ++data_reuse ){
+       x =
+        // X direction
+       (
+        (
+            local_flat[ Index3D( BLOCK_DIM, BLOCK_DIM, local_index_x - 1, local_index_y, 0) ] +
+            local_flat[ Index3D( BLOCK_DIM, BLOCK_DIM, local_index_x + 1, local_index_y, 0) ]
+            -
+            tijk
+        ) / dxsq
+        +
+        // Y direction
+        (
+            local_flat[ Index3D( BLOCK_DIM, BLOCK_DIM, local_index_x, local_index_y - 1, 0) ] +
+            local_flat[ Index3D( BLOCK_DIM, BLOCK_DIM, local_index_x, local_index_y + 1, 0) ]
+            -
+            tijk
+        ) / dysq
+        +
+        // Z direction
+        (
+            zlow +
+            zhigh
+            -
+            tijk
+        ) / dzsq
+       );
+      }
+      global_data_out[ inner_offset + threadIdx.x + nx * threadIdx.y ] = x;
+    }
+    __syncthreads();
   }
 }
 
@@ -183,7 +227,7 @@ void __host__ divergence_float_gpu(FieldT* f, float dx, float dy, float dz){
      *          - grab x, y extents ( we should get the entire memory window ) ?
      *          - decide how many 16x16 blocks are needed to tile the base plane.
      *            This might be a value that is not a multiple of 16.
-     */
+     **/
     using namespace SpatialOps::structured;
     cudaError err;
 
@@ -192,50 +236,62 @@ void __host__ divergence_float_gpu(FieldT* f, float dx, float dy, float dz){
     unsigned int blockx = ( extent[0] / BLOCK_DIM + ( extent[0] % BLOCK_DIM == 0 ? 0 : 1) );
     unsigned int blocky = ( extent[1] / BLOCK_DIM + ( extent[1] % BLOCK_DIM == 0 ? 0 : 1) );
 
+    unsigned int blockx_op2 = ( ( extent[0] - 2) / (INNER_BLOCK_DIM) +
+        ( ( extent[0] - 2 ) % (INNER_BLOCK_DIM) == 0 ? 0 : 1) );
+    unsigned int blocky_op2 = ( ( extent[1] - 2) / (INNER_BLOCK_DIM) +
+        ( ( extent[1] - 2 ) % (INNER_BLOCK_DIM) == 0 ? 0 : 1) );
+
     float* h_workspace = (float*)malloc(f->get_data_size());
     float* d_workspace;
+
     cudaMalloc((void**)&d_workspace, f->get_data_size() );
+    cudaMemset(d_workspace, 0, f->get_data_size());
     cudaMemcpy((void*)d_workspace, (void*)f->get_ext_pointer(), f->get_data_size(), cudaMemcpyDeviceToDevice );
 
-    dim3 dimBlock( BLOCK_DIM + 2, BLOCK_DIM + 2 );
+    dim3 dimBlock( BLOCK_DIM, BLOCK_DIM );
     dim3 dimGrid( blockx, blocky );
 
-    std::cout << "Executing with " << blockx << " X " << blocky << " block grid" << std::endl;
-    std::cout << "               " << BLOCK_DIM << " X " << BLOCK_DIM << " block threads\n";
+    dim3 dimBlock_op2( BLOCK_DIM, BLOCK_DIM );
+    dim3 dimGrid_op2( blockx_op2, blocky_op2 );
 
-    std::cout << "Extent: " << extent[0] << " " << extent[1] << " " << extent[2] << std::endl;
+    //std::cout << "Executing with " << blockx_op2 << " X " << blocky_op2 << " block grid" << std::endl;
+    //std::cout << "               " << BLOCK_DIM << " X " << BLOCK_DIM << " block threads\n";
+
+    //std::cout << "Extent: " << extent[0] << " " << extent[1] << " " << extent[2] << std::endl;
+
     if( cudaSuccess != ( err = cudaSetDevice( f->device_index() ) ) ){
       throw( std::runtime_error( cudaGetErrorString(err) ) );
     }
 
-    clock_t tick = clock();
-    for( int i = 0; i < 1e3; ++i ){
-      _div_float_opt2<<<dimGrid, dimBlock, 0, 0>>>( d_workspace,
-                                                  f->get_ext_pointer(),
-                                                  dx, dy, dz,
-                                                  extent[0], extent[1], extent[2]);
+    _div_float_opt1<<<dimGrid, dimBlock, 0, 0>>>( d_workspace,
+                                                f->get_ext_pointer(),
+                                                dx, dy, dz,
+                                                extent[0], extent[1], extent[2] );
 
-      cudaMemcpy(h_workspace, f->get_ext_pointer(), f->get_data_size(), cudaMemcpyDeviceToHost);
-    }
-    clock_t tock = clock() - tick;
-
-    //cudaThreadSynchronize();
+    /*
+    _div_float_opt2<<<dimGrid_op2, dimBlock_op2, 0, 0>>>( d_workspace,
+                                                f->get_ext_pointer(),
+                                                dx, dy, dz,
+                                                extent[0], extent[1], extent[2] );
+    */
+    cudaThreadSynchronize();
+    //cudaMemcpy(h_workspace, f->get_ext_pointer(), f->get_data_size(), cudaMemcpyDeviceToHost);
     cudaFree(d_workspace);
 
-
-    std::cout << "Elapsed: " << (double)tock / (double)CLOCKS_PER_SEC << std::endl;
-
-    //Dump first computed plane in the z axis
+    /*
     std::ofstream file;
     file.open("cudaout.txt");
     for(int i = 0; i < extent[0]; ++i){
       for( int j = 0; j < extent[1]; ++j){
-        float q = h_workspace[Index3D(extent[0], extent[1], i, j, 1)];
-        file << std::setw(8) << q << " ";
+        float q = h_workspace[ Index3D(extent[0], extent[1], i, j, 1) ];
+        file.precision(8);
+        file << std::setw(12) << q << " ";
       }
       file << std::endl;
     }
     file.close();
+    */
+
 }
 
 template< class FieldT >
--- spatialops/structured/test/ExternalStencilTest.cu
+++ spatialops/structured/test/ExternalStencilTest.cu
@@ -18,53 +18,69 @@ using namespace structured;
 using namespace Point;
 using namespace ema::cuda;
 
-#define SMALL 16
-//#define Index3D(_nx,_ny,_i,_j,_k) ((_i)+_nx*((_j)+_ny*(_k)))
-//#define Index3DG(_nx, _ny, _g, _i, _j, _k) (((_i)+(_g)) + _nx*(((_j)+(_g) + _ny*((_k)+(_g)) ))
+#define SMALL 64
+#define HEIGHT 64
+#define ITERATIONS 1
 
 void stencil7_cpu( float* data_in, float* data_out, int nx, int ny, int nz ){
-  int dx = 1, dy = 1, dz = 1;
-  for( int i = 1; i < SMALL-1; ++i)
-    for( int j = 1; j < SMALL-1; ++j)
-      for( int k = 1; k < SMALL-1; ++k){
-        float tijk = 2*data_in[ Index3D( nx, ny, i,j,k) ];
+  float dx = 1, dy = 1, dz = 1;
+
+  float dxsq = dx*dx;
+  //float dysq = dy*dy;
+  //float dzsq = dz*dz;
+
+  for( int k = 1; k < HEIGHT-1; ++k)
+   for( int j = 1; j < SMALL-1; ++j)
+    for( int i = 1; i < SMALL-1; ++i){
+      for( int data_reuse = 0; data_reuse < 1; ++data_reuse ){
+        float tijk = 2*data_in[ Index3D( nx, ny, i, j, k) ];
         data_out[Index3D (nx, ny, i, j, k)] =
-        // X direction
         (
-            data_in[Index3D (nx, ny, i - 1, j, k)] +
-            data_in[Index3D (nx, ny, i + 1, j, k)] -
-            tijk
-        ) / ( dx * dx )
-        +
-        // Y direction
-        (
-            data_in[Index3D (nx, ny, i, j - 1, k)] +
-            data_in[Index3D (nx, ny, i, j + 1, k)] -
-            tijk
-        ) / ( dy * dy )
-        +
-        // Z direction
-        (
-            data_in[Index3D (nx, ny, i, j, k - 1)] +
-            data_in[Index3D (nx, ny, i, j, k + 1)] -
-            tijk
-        ) / ( dz * dz );
+          // X direction
+          (
+              data_in[Index3D (nx, ny, i - 1, j, k)] +
+              data_in[Index3D (nx, ny, i + 1, j, k)]
+              -
+              tijk
+          )
+          +
+          // Y direction
+          (
+              data_in[Index3D (nx, ny, i, j - 1, k)] +
+              data_in[Index3D (nx, ny, i, j + 1, k)]
+              -
+              tijk
+          )
+          +
+          // Z direction
+          (
+              data_in[Index3D (nx, ny, i, j, k - 1)] +
+              data_in[Index3D (nx, ny, i, j, k + 1)]
+              -
+              tijk
+          )
+        ) / dxsq
+        ;
       }
+    }
 }
 
 int main(int argc, char** argv){
-  const IntVec sset(SMALL,SMALL,SMALL);
+  const IntVec sset(SMALL,SMALL,HEIGHT);
   const IntVec mset(64,64,64);
   const IntVec lset(128,128,128);
 
-  float* svals = new float[SMALL*SMALL*SMALL];
-  float* data_out = new float[SMALL*SMALL*SMALL];
+  float* svals = new float[SMALL*SMALL*HEIGHT];
+  float* data_out = new float[SMALL*SMALL*HEIGHT];
+
+  clock_t tick;
+  clock_t tock;
 
   srand(0);
 
   for( int i = 1; i < SMALL-1; ++i)
     for( int j = 1; j < SMALL-1; ++j)
-      for( int k = 1; k < SMALL-1; ++k)
+      for( int k = 1; k < HEIGHT-1; ++k)
          svals[Index3D(SMALL,SMALL, i,j,k)] = (float)rand() / (float)RAND_MAX;
 
   CUDADeviceInterface& CDI = CUDADeviceInterface::self();
@@ -77,20 +93,19 @@ int main(int argc, char** argv){
   PointFloatField pff(swin, NULL, InternalStorage, LOCAL_RAM, 0);
   PointFloatField spff(swin, svals, InternalStorage, EXTERNAL_CUDA_GPU, 0);
 
-  CDI.print_device_info();
+  //CDI.print_device_info();
 
-  clock_t tick = clock();
-  for( int i = 0; i < 1e3; ++i )
-  stencil7_cpu(svals, data_out, SMALL, SMALL, SMALL);
-  clock_t tock = clock() - tick;
 
-  for(int i = 0; i < 2; i++){
-    for(int j = 0; j < 2; j++)
-      std::cout << Index3DG(4,4,1,j,i,0) << " ";
-    std::cout << std::endl;
+  double avgcpu = 0;
+  for( int p = 0; p < ITERATIONS; ++p){
+    tick = clock();
+    stencil7_cpu(svals, data_out, SMALL, SMALL, HEIGHT);
+    clock_t tock = clock() - tick;
+    avgcpu += tock;
   }
+  avgcpu /= ITERATIONS;
 
-  std::cout << "Elapsed: " << (double)tock / (double)CLOCKS_PER_SEC << std::endl;
+  std::cout << "Elapsed: " << (double)avgcpu / (double)CLOCKS_PER_SEC << std::endl;
 
   //Dump first computed plane in the z axis
   std::ofstream file;
@@ -98,13 +113,25 @@ int main(int argc, char** argv){
   for(int i = 0; i < SMALL; ++i){
     for( int j = 0; j < SMALL; ++j){
       float q = data_out[Index3D(SMALL, SMALL, i, j, 1)];
-      file << std::setw(8) << q << " ";
+      file.precision(8);
+      file << std::setw(12) << q << " ";
     }
     file << std::endl;
   }
   file.close();
 
-  divergence_float_gpu(&spff, 1, 1, 1);
+  double avggpu=0;
+  for(int j = 0; j < ITERATIONS; ++j){
+    tick = clock();
+    divergence_float_gpu(&spff, 1, 1, 1);
+    tock = clock() - tick;
+    avggpu += tock;
+  }
+  avggpu /= ITERATIONS;
+
+  std::cout << "Average elapsed ( opt2 ): " << (double)avggpu / (double)CLOCKS_PER_SEC << std::endl;
+  std::cout << "Speedup: " << avgcpu / avggpu << std::endl;
+
   pff = spff;
 
   delete[] svals;
