8ed1330cfbb46c6f451b8f5c8bbc29b1f9b9d63c
James C. Sutherland <James.Sutherland@utah.edu>
2013-07-08 08:34:28 -0600

    Create a "Stencil4_def.h" file that contains the class definition so that we can have other instantiations of this class other than what Stencil4.cpp provides.  This was needed by the LBMS code.

--- spatialops/structured/stencil/Stencil4.cpp
+++ spatialops/structured/stencil/Stencil4.cpp
@@ -20,60 +20,10 @@
  * IN THE SOFTWARE.
  */
 
-#include <spatialops/SpatialOpsConfigure.h>
-
-#ifdef ENABLE_CUDA
-#include "CudaStencil4.h"
-#else
-#include "Stencil4.h"
-#endif
-
-#include <spatialops/structured/FVStaggeredFieldTypes.h>
-#include <spatialops/structured/FVTools.h>
-#include <spatialops/structured/MemoryTypes.h>
-#include <spatialops/Nebo.h>
+#include "Stencil4_def.h"
 
 namespace SpatialOps{
 namespace structured{
-
-  //==================================================================
-
-  template< typename OpT, typename SrcT, typename DestT >
-  Stencil4<OpT,SrcT,DestT>::
-  Stencil4( const double coef1,
-            const double coef2,
-            const double coef3,
-            const double coef4 )
-    : coef1_( coef1 ),
-      coef2_( coef2 ),
-      coef3_( coef3 ),
-      coef4_( coef4 ),
-      coefCollection_( build_four_point_coef_collection(coef1, coef2, coef3, coef4) )
-  {}
-
-  //------------------------------------------------------------------
-
-  template< typename OpT, typename SrcT, typename DestT >
-  void Stencil4<OpT,SrcT,DestT>::apply_to_field( const SrcT& src, DestT& dest ) const
-  {   switch( dest.memory_device_type() ){
-      case LOCAL_RAM:
-        dest <<= operator()(src);
-        break;
-#ifdef ENABLE_CUDA
-      case EXTERNAL_CUDA_GPU:
-        cuda_stencil_4_apply_to_field_helper<OpT,SrcT,DestT>( src, dest, coef1_, coef2_, coef3_, coef4_ );
-        break;
-#endif
-      default:{
-        std::ostringstream msg;
-        msg << "Destination field has unsupported device type ( "
-            << DeviceTypeTools::get_memory_type_description( dest.memory_device_type() )
-            << " )\n";
-        msg << "\t - " << __FILE__ << " : " << __LINE__;
-        throw(std::runtime_error(msg.str()));
-      }
-    }
-  }
 
   //==================================================================
   // Explicit template instantiation
