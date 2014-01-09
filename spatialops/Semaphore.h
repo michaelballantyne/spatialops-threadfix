d36b97b4a944730018275791c0453f86458bea78
James C. Sutherland <James.Sutherland@utah.edu>
2014-01-09 16:52:59 -0700

    Add some documentation to Semaphore.

--- spatialops/Semaphore.h
+++ spatialops/Semaphore.h
@@ -4,38 +4,49 @@
 #include <boost/thread/mutex.hpp>
 #include <boost/thread/condition_variable.hpp>
 
-/**
- * Implemented to replace boost::interprocess::interprocess_semaphore due to posix semaphores bug in glibc:
- * http://sourceware.org/bugzilla/show_bug.cgi?id=12674
- *
- * However, there are many more bugs in glibc! On some (non x86 / x86_64) platforms, this one could bite us:
- * http://sourceware.org/bugzilla/show_bug.cgi?id=13690
- */
 namespace SpatialOps{
-    struct Semaphore {
-        public:
-            Semaphore(int initial) {
-                val = initial;
-            }
 
-            inline void post() {
-                boost::lock_guard<boost::mutex> lock(mut);
-                val++;
-                cond.notify_one();
-            }
+  /**
+   * @struct Semaphore
+   *
+   * @brief Provide resource management for multithreaded situations.
+   *
+   * Implemented to replace boost::interprocess::interprocess_semaphore due
+   * to posix semaphores bug in glibc: http://sourceware.org/bugzilla/show_bug.cgi?id=12674
+   *
+   * However, there are many more bugs in glibc! On some (non x86 / x86_64)
+   * platforms, this one could bite us: http://sourceware.org/bugzilla/show_bug.cgi?id=13690
+   */
+  struct Semaphore {
+  public:
 
-            inline void wait() {
-                boost::unique_lock<boost::mutex> lock(mut);
-                while (val <= 0) {
-                    cond.wait(lock);
-                }
-                val--;
-            }
+    /**
+     * @param initial the initial number of resources available
+     */
+    Semaphore(int initial=0) : val_(initial){}
 
-        private:
-            boost::condition_variable cond;
-            boost::mutex mut;
-            int val;
-    };
+    /** @brief release a resource */
+    inline void post() {
+      boost::lock_guard<boost::mutex> lock(mut_);
+      ++val_;
+      cond_.notify_one();
+    }
+
+    /**
+     * @brief Wait until a resource is available (a call to \c post is made).
+     */
+    inline void wait() {
+      boost::unique_lock<boost::mutex> lock(mut_);
+      while (val_ <= 0) {
+        cond_.wait(lock);
+      }
+      --val_;
+    }
+
+  private:
+    boost::condition_variable cond_;
+    boost::mutex mut_;
+    int val_;
+  };
 }
 #endif // Nebo_Semaphore_h
