dc2e3e9e509f9a0e23504901516bc2af18881690
Abhishek Bagusetty <u0794494@aurora.crsim.utah.edu>
2014-06-02 10:18:11 -0600

    Getting rid of MemoryTypes that describes the hardware device type and replacing it with CPU_INDEX & GPU_INDEX.

--- spatialops/structured/MemoryTypes.h
+++ spatialops/structured/MemoryTypes.h
@@ -10,44 +10,31 @@
 #include <string>
 namespace SpatialOps{
 
-  /**
-   *  \enum MemoryType
-   *  \brief Enumerates the supported memory management strategies.
-   */
-   enum MemoryType {
-     LOCAL_RAM,           ///< Locally allocated memory: standard CPU attached RAM.
-     LOCAL_VECTOR_INST,   ///< Future use, locally allocated memory: SSE type instruction.
-     EXTERNAL_CUDA_GPU,   ///< Externally allocated memory: NVIDIA device via CUDA.
-     EXTERNAL_OPENCL_GPU, ///< Future use, externally allocated memory: Generic GPU device via OpenCL.
-     EXTERNAL_INTEL_MIK,  ///< Future use, externally allocated memory: Intel MIK chip
-     DEBUG_TEST_OPT,      ///< used for testing error conditions related to MemoryType
-     UNKNOWN              ///< error state
-   };
+#define CPU_INDEX -1
+#define GPU_INDEX  0
+
+#define IS_CPU_INDEX(INDEX)   (INDEX == CPU_INDEX)
+#define IS_GPU_INDEX(INDEX)   (INDEX >= GPU_INDEX)
+#define IS_VALID_INDEX(INDEX) (INDEX >= CPU_INDEX)
 
   /**
    *  \class DeviceTypeTools
    *  \brief Provide descriptions
    */
   namespace DeviceTypeTools {
-  inline std::string get_memory_type_description( MemoryType m )
+  inline std::string get_memory_type_description( short int deviceIndex )
   {
-    switch ( m ){
-    case LOCAL_RAM:
-      return std::string("Locally allocated, generic system RAM.");
-    case LOCAL_VECTOR_INST:
-      return std::string("Locally allocated, vector device or CPU extension.");
-    case EXTERNAL_CUDA_GPU:
-      return std::string("Externally allocated, CUDA GPU device.");
-    case EXTERNAL_OPENCL_GPU:
-      return std::string("Externally allocated, OpenCL GPU device.");
-    case EXTERNAL_INTEL_MIK:
-      return std::string("Externally allocated, intel MIK device.");
-    case DEBUG_TEST_OPT:
-      return std::string("Debugging value -- Are you sure this is what you want to be using?");
-    default:
-      return std::string("Unknown or Invalid");
-    }
+    if( deviceIndex == CPU_INDEX ){
+       return std::string("(Locally allocated, generic system RAM)");
+     }
+     else if( IS_GPU_INDEX(deviceIndex) ){
+       return std::string("(Externally allocated, CUDA GPU device)");
+     }
+     else{
+       return std::string("(Unknown or Invalid)");
+     }
   }
+
   } // namespace DeviceTypeTools
 } // namespace SpatialOps
 
