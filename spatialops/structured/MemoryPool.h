5ddef927275f6ac4c073e38d87a05e7409c049f4
James C. Sutherland <James.Sutherland@utah.edu>
2014-07-21 12:57:47 -0600

    Improve documentation to MemoryPool.

--- spatialops/structured/MemoryPool.h
+++ spatialops/structured/MemoryPool.h
@@ -58,9 +58,35 @@ class Pool{
 
  public:
 
+  /**
+   * @brief Obtain a pointer to a block of memory with the requested size.
+   *        If the requested size is zero, this returns a NULL pointer.
+   * @param deviceLocation the location where the memory is requested
+   * @param n the size of the block of memory
+   *
+   * Example: return a pointer to a block of memory for 5 doubles
+   * \code
+   * double* my5Doubles = Pool<double>::get( CPU_INDEX, 5 );
+   * \endcode
+   */
   static T* get( const short int deviceLocation, const size_t n );
-  static void put( const short int deviceLocation, T* );
+
+  /**
+   * @brief Return the requested block of memory to the pool. This should only
+   *        be done for memory requested from the pool to begin with!
+   * @param deviceLocation the location to return it to
+   * @param t the pointer to the block of memory.
+   */
+  static void put( const short int deviceLocation, T* t );
+
+  /**
+   * @return the number of active fields in the pool on CPU
+   */
   static size_t active();
+
+  /**
+   * @return the maximum number of active fields in the pool on CPU
+   */
   static size_t total();
 };
 
